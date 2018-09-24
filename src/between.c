2a1fa8d0c69f7c47438a89599d88c6f1fbaa7724
Philippe Chataignon <philippe.chataignon@dgfip.finances.gouv.fr>
2018-09-24 15:11:18 +0200

    Readd (int|double)_both functions in between
    This functions are needed for correct NA handling. between(NA, ...) returns NA.
    
    tests 1529.* are correct with this.

--- src/between.c
+++ src/between.c
@@ -19,6 +19,22 @@ static Rboolean int_lower_open(SEXP x, R_len_t i) {
   return (INTEGER(x)[i] == NA_INTEGER || (double)INTEGER(x)[i] > l ? NA_LOGICAL : FALSE);
 }
 
+static Rboolean int_both_closed(SEXP x, R_len_t i) {
+  return (INTEGER(x)[i] == NA_INTEGER ? NA_LOGICAL : ((double)INTEGER(x)[i] >= l && (double)INTEGER(x)[i] <= u));
+}
+
+static Rboolean int_both_open(SEXP x, R_len_t i) {
+  return (INTEGER(x)[i] == NA_INTEGER ? NA_LOGICAL : ((double)INTEGER(x)[i] > l && (double)INTEGER(x)[i] < u));
+}
+
+static Rboolean int_both_closed_open(SEXP x, R_len_t i) {
+  return (INTEGER(x)[i] == NA_INTEGER ? NA_LOGICAL : ((double)INTEGER(x)[i] >= l && (double)INTEGER(x)[i] < u));
+}
+
+static Rboolean int_both_open_closed(SEXP x, R_len_t i) {
+  return (INTEGER(x)[i] == NA_INTEGER ? NA_LOGICAL : ((double)INTEGER(x)[i] > l && (double)INTEGER(x)[i] <= u));
+}
+
 static Rboolean double_upper_closed(SEXP x, R_len_t i) {
   return (ISNAN(REAL(x)[i]) || REAL(x)[i] <= u ? NA_LOGICAL : FALSE);
 }
@@ -35,12 +51,28 @@ static Rboolean double_lower_open(SEXP x, R_len_t i) {
   return (ISNAN(REAL(x)[i]) || REAL(x)[i] > l ? NA_LOGICAL : FALSE);
 }
 
+static Rboolean double_both_closed(SEXP x, R_len_t i) {
+  return (ISNAN(REAL(x)[i]) ? NA_LOGICAL : (REAL(x)[i] >= l && REAL(x)[i] <= u));
+}
+
+static Rboolean double_both_open(SEXP x, R_len_t i) {
+  return (ISNAN(REAL(x)[i]) ? NA_LOGICAL : (REAL(x)[i] > l && REAL(x)[i] < u));
+}
+
+static Rboolean double_both_open_closed(SEXP x, R_len_t i) {
+  return (ISNAN(REAL(x)[i]) ? NA_LOGICAL : (REAL(x)[i] > l && REAL(x)[i] <= u));
+}
+
+static Rboolean double_both_closed_open(SEXP x, R_len_t i) {
+  return (ISNAN(REAL(x)[i]) ? NA_LOGICAL : (REAL(x)[i] >= l && REAL(x)[i] < u));
+}
+
 SEXP between(SEXP x, SEXP lower, SEXP upper, SEXP bounds, SEXP right) {
 
   R_len_t i, nx = length(x), nl = length(lower), nu = length(upper);
   l = 0.0; u = 0.0;
   SEXP ans;
-  Rboolean (*flower)(), (*fupper)();
+  Rboolean (*flower)(), (*fupper)(), (*fboth)();
   if (!nx || !nl || !nu)
     return (allocVector(LGLSXP, 0));
   if (nl != 1 && nl != nx)
@@ -68,24 +100,28 @@ SEXP between(SEXP x, SEXP lower, SEXP upper, SEXP bounds, SEXP right) {
 
   if (LOGICAL(bounds)[0]) {
     if (LOGICAL(right)[0]) {
+      flower = isInteger(x) ? &int_lower_closed : &double_lower_closed;
       fupper = isInteger(x) ? &int_upper_closed : &double_upper_closed;
-      flower = isInteger(x) ? &int_lower_closed : &double_lower_closed;
+      fboth  = isInteger(x) ? &int_both_closed  : &double_both_closed;
     } else {
-      fupper = isInteger(x) ? &int_upper_open : &double_upper_open;
       flower = isInteger(x) ? &int_lower_closed : &double_lower_closed;
+      fupper = isInteger(x) ? &int_upper_open : &double_upper_open;
+      fboth  = isInteger(x) ? &int_both_closed_open  : &double_both_closed_open;
     }
   } else {
     if (LOGICAL(right)[0]) {
+      flower = isInteger(x) ? &int_lower_open : &double_lower_open;
       fupper = isInteger(x) ? &int_upper_closed : &double_upper_closed;
-      flower = isInteger(x) ? &int_lower_open : &double_lower_open;
+      fboth  = isInteger(x) ? &int_both_open_closed  : &double_both_open_closed;
     } else {
-      fupper = isInteger(x) ? &int_upper_open : &double_upper_open;
       flower = isInteger(x) ? &int_lower_open : &double_lower_open;
+      fupper = isInteger(x) ? &int_upper_open : &double_upper_open;
+      fboth  = isInteger(x) ? &int_both_open  : &double_both_open;
     }
   }
 
-  if ( ISNAN(REAL(lower)[0]) ) {
-    if ( ISNAN(REAL(upper)[0]) ) {
+  if (ISNAN(REAL(lower)[0]) ) {
+    if (ISNAN(REAL(upper)[0]) ) {
       #pragma omp parallel for num_threads(getDTthreads())
       for (i=0; i<nx; i++) LOGICAL(ans)[i] = NA_LOGICAL;
     } else {
@@ -98,7 +134,7 @@ SEXP between(SEXP x, SEXP lower, SEXP upper, SEXP bounds, SEXP right) {
       for (i=0; i<nx; i++) LOGICAL(ans)[i] = flower(x, i);
     } else {
       #pragma omp parallel for num_threads(getDTthreads())
-      for (i=0; i<nx; i++) LOGICAL(ans)[i] = flower(x, i) && fupper(x, i);
+      for (i=0; i<nx; i++) LOGICAL(ans)[i] = fboth(x, i);
     }
   }
   UNPROTECT(nprotect);
